1209390cd9f0b75740b8e6eeb5d78362eba9494c
aidai <izumisoft1@mail.ru>
2022-12-30 00:19:06 +0900

    Initial commit

---
+++ JDBCandHibernateProject-master/src/main/java/com/peaksoft/App.java
@@ -0,0 +1,19 @@
+package com.peaksoft;
+
+
+import com.peaksoft.service.UserServiceImpl;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+//       userService.createUsersTable();
+//        userService.dropUsersTable();
+//          userService.saveUser("Dilnaz","Tolonbaeva", (byte) 16);
+//        userService.removeUserById(2);
+//       System.out.println(userService.getAllUsers());
+//        userService.cleanUsersTable();
+    }
+}
